409f2ca33f4171aaba81260e2d8ada34e6a13aa7
jerevallejo <44449584+jerevallejo@users.noreply.github.com>
2019-10-06 11:53:15 -0300

    Add more examples in  BootStrap's class

--- grails-app/init/lookapp/backend/BootStrap.groovy
+++ grails-app/init/lookapp/backend/BootStrap.groovy
@@ -65,6 +65,12 @@ class BootStrap {
                 service2.cost=250
                 service2.save()
 
+                Service service3 =new Service()
+                service3.name="peinado"
+                service3.duration=30
+                service3.cost=150
+                service3.save()
+
                 Professional professional=new Professional()
                 professional.name="Pedro"
                 professional.services=new ArrayList<Service>()
@@ -79,10 +85,20 @@ class BootStrap {
                 cliente.primaryPhone= 12312
                 cliente.save()
 
+                Client cliente1 = new Client()
+                cliente1.name = "cliente2"
+                cliente1.surname = "apellido1"
+                cliente1.DNI= "1312315"
+                cliente1.primaryPhone= 12312
+                cliente1.secondPhone= 123333
+                cliente1.save()
+
                 Appointment turn=new Appointment()
                 turn.local="casa"
                 turn.client = cliente
-                turn.dayHour= new Date()
+                Calendar cal = Calendar.getInstance()
+                cal.set(2019,9,29)
+                turn.dayHour= cal.getTime()
                 turn.services=new ArrayList<Service>()
                 turn.services.add(service)
                 turn.appointmentStatus = AppointmentStatus.OPEN
@@ -90,12 +106,24 @@ class BootStrap {
 
                 Appointment turn1=new Appointment()
                 turn1.local="San Miguel"
-                turn1.client = cliente
-                turn1.dayHour= new Date()
+                turn1.client = cliente1
+                Calendar cal1 = Calendar.getInstance()
+                cal1.set(2019,9,28)
+                turn1.dayHour= cal1.getTime()
                 turn1.services=new ArrayList<Service>()
                 turn1.services.add(service2)
                 turn1.appointmentStatus = AppointmentStatus.OPEN
                 turn1.save()
+
+                Appointment turn2=new Appointment()
+                turn2.local="San Miguel"
+                Calendar cal2 = Calendar.getInstance()
+                cal2.set(2019,9, 30)
+                turn2.dayHour= cal2.getTime()
+                turn2.services=new ArrayList<Service>()
+                turn2.services.add(service3)
+                turn2.appointmentStatus = AppointmentStatus.OPEN
+                turn2.save()
 
             }
         }

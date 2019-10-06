855a28c4d8a4eb91e97ce0d4decd3f6cff1c3555
Walter Pereyra Lopez <walter.pereyra@mercadolibre.com>
2019-10-05 22:16:25 -0300

    Client add to appointment in bootstrap

--- grails-app/init/lookapp/backend/BootStrap.groovy
+++ grails-app/init/lookapp/backend/BootStrap.groovy
@@ -81,6 +81,7 @@ class BootStrap {
 
                 Appointment turn=new Appointment()
                 turn.local="casa"
+                turn.client = cliente
                 turn.dayHour= new Date()
                 turn.services=new ArrayList<Service>()
                 turn.services.add(service)
@@ -89,6 +90,7 @@ class BootStrap {
 
                 Appointment turn1=new Appointment()
                 turn1.local="San Miguel"
+                turn1.client = cliente
                 turn1.dayHour= new Date()
                 turn1.services=new ArrayList<Service>()
                 turn1.services.add(service2)

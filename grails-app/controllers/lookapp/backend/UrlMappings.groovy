06484f2dfb54cd210b6fd65acaa3b84a105bfb63
Bruno Capecce <bcapecce@gmail.com>
2019-10-06 23:54:42 -0300

    add availableprofessionals

--- grails-app/controllers/lookapp/backend/UrlMappings.groovy
+++ grails-app/controllers/lookapp/backend/UrlMappings.groovy
@@ -5,8 +5,13 @@ class UrlMappings {
     static mappings = {
         "/"(controller: 'application', action:'index')
         "/login"(controller: 'login', action:'login',method:'POST')
-        "/appointments/$id/cancel"(controller: 'appointmentStatus', action:'cancel')
-        "/appointments/$id/paid"(controller: 'appointmentStatus', action:'paid')
+        "/appointments"(controller: 'appointment', action:'list',method:'GET')
+        "/appointments/$id"(controller: 'appointment', action:'show',method:'GET')
+        "/appointments"(controller: 'appointment', action:'create',method:'POST')
+        "/appointments/$id"(controller: 'appointment', action:'update',method:'PUT')
+        "/appointments/$id/cancel"(controller: 'appointment', action:'cancel',method:'POST')
+        "/appointments/$id/paid"(controller: 'appointment', action:'paid',method:'POST')
+        "/professionals/search"(controller: 'appointment', action:'searchProfessionals')
 
         "500"(controller: 'application', action:'error')
         "404"(view: '/notFound')
--- grails-app/domain/lookapp/backend/Day.groovy
+++ grails-app/domain/lookapp/backend/Day.groovy
@@ -1,6 +1,13 @@
 package lookapp.backend
 
 enum Day {
-    MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+    SUNDAY(1),MONDAY(2),TUESDAY(3),WEDNESDAY(4),THURSDAY(5),FRIDAY(6),SATURDAY(7)
+    private final int value
+    Day(int value){
+        this.value=value
+    }
+    static Day byId(int id) {
+        values().find { it.value == id }
+    }
 }
 
